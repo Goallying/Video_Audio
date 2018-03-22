1d632081ba86f552297d23fab0f16e7d1ef51ad8
朱来飞 <Goallying@zhulaifeideMacBook-Pro.local>
2018-03-22 23:31:24 +0800

    Initial commit

---
+++ Video_Audio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Video_Audio
+//
+//  Created by 朱来飞 on 2018/3/22.
+//  Copyright © 2018年 朱来飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Video_Audio/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  Video_Audio
+//
+//  Created by 朱来飞 on 2018/3/22.
+//  Copyright © 2018年 朱来飞. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#define kVideoURL @"http://bos.nj.bpc.baidu.com/tieba-smallvideo/11772_3c435014fb2dd9a5fd56a57cc369f6a0.mp4"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)playLocalVideo:(UIButton *)sender {
+    
+}
+- (IBAction)playVideoOnlie:(UIButton *)sender {
+}
+
+@end
